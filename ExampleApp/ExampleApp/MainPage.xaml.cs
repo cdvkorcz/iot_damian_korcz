7ce0ae117dc0f00582423cfa008c8e75d6dea356
Damian Korcz <koczur11@gmail.com>
2020-10-15 17:03:07 +0200

    Projekt testowy srodowiska

---
+++ ExampleApp/ExampleApp/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExampleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var content = (StackLayout)this.Content;
+        }
+
+        public class App : Application
+        {
+            public App()
+            {
+                MainPage = new MainPage();
+            }
+ 
+        }
+    }
+}
